fd330edff9da87813072627346b8d7b91b4e1961
Moon-YJ <tunjeong@gmail.com>
2024-01-11 15:12:48 +0900

    post DB 내용변경에 따른 수정

---
+++ app/post/page.jsx
@@ -0,0 +1,30 @@
+'use client';
+import styles from './post.module.scss';
+import Link from 'next/link';
+import { useState, useEffect } from 'react';
+import clsx from 'clsx';
+
+export default function Post() {
+	const [Post, setPost] = useState([]);
+
+	useEffect(() => {
+		fetch('/api/requestPost')
+			.then((data) => data.json())
+			.then((json) => setPost(json.result));
+	}, []);
+
+	return (
+		<div className={clsx(styles.post)}>
+			<h1>Post List</h1>
+			{Post.map((post) => {
+				return (
+					<article key={post.id}>
+						<h2>
+							<Link href={`/post/${post.id}`}>{post.title}</Link>
+						</h2>
+					</article>
+				);
+			})}
+		</div>
+	);
+}
