2ce0f9e7e0f28b6f403eb6eafc2bbd6eb91c62bf
Moon-YJ <tunjeong@gmail.com>
2024-01-11 15:57:01 +0900

    input 입력 내용 실시간으로 state에 저장

--- app/post/[id]/page.jsx
+++ app/post/[id]/page.jsx
@@ -1,21 +1,31 @@
 'use client';
 import { useState, useEffect } from 'react';
+import styles from './detail.module.scss';
+import clsx from 'clsx';
 
 export default function PostDetail({ params }) {
-  const { id } = params;
-  const [PostEl, setPostEl] = useState(null);
+	const { id } = params;
+	const [PostEl, setPostEl] = useState(null);
 
-  useEffect(() => {
-    fetch(`/api/requestPost/${id}`)
-      .then((data) => data.json())
-      .then((json) => {
-        console.log(json);
-      });
-  }, [id]);
+	useEffect(() => {
+		fetch(`/api/requestPost/${id}`)
+			.then((data) => data.json())
+			.then((json) => {
+				console.log(json);
+				setPostEl(json.result);
+			});
+	}, [id]);
 
-  return (
-    <div>
-      <h2>{id}번째 게시글 상세페이지</h2>
-    </div>
-  );
+	return (
+		<section className={clsx(styles.detail)}>
+			<article>
+				<h2>{PostEl?.title}</h2>
+				<p>{PostEl?.content}</p>
+			</article>
+			<nav>
+				<button>Edit</button>
+				<button>Delete</button>
+			</nav>
+		</section>
+	);
 }
