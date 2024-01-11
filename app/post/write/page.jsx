48b09e6434924e4ec1c7501782a42117712bb201
Moon-YJ <tunjeong@gmail.com>
2024-01-11 16:21:40 +0900

    클라이언트에 전달한 정보값을 parsing한 뒤 성공응답객체를 다시 클라이언트로 전달

--- app/post/write/page.jsx
+++ app/post/write/page.jsx
@@ -13,10 +13,27 @@ export default function Write() {
 		setPost({ ...Post, [name]: value });
 	};
 
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const response = await fetch('/api/requestPost', {
+			method: 'POST',
+			headers: { 'Content-type': 'application/json' },
+			body: JSON.stringify(Post),
+		});
+
+		if (response.ok) {
+			console.log(response);
+			alert('글 저장에 성공했습니다.');
+		} else {
+			console.log(response);
+			alert('글 저장에 실패했습니다.');
+		}
+	};
+
 	return (
 		<div className={clsx(styles.write)}>
 			<h1>Write Post</h1>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<input
 					type='text'
 					placeholder='title'
@@ -31,6 +48,7 @@ export default function Write() {
 					value={Post.content}
 					onChange={handleChange}
 				></textarea>
+				<button>저장</button>
 			</form>
 		</div>
 	);
